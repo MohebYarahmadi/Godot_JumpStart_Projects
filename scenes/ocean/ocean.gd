0d671952cec3f520f657f4eb0db7920bfbd754a1
MohebYarahmadi <moheb.yarahmadi@gmail.com>
2026-02-20 10:30:30 +0330

    Friday Practice

--- scenes/ocean/ocean.gd
+++ scenes/ocean/ocean.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 @onready var plane: Sprite2D = $plane
+@onready var carrier: Sprite2D = $carrier
 @onready var helicopter: Sprite2D = $carrier/helicopter
 
 
@@ -13,4 +14,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	plane.position.x += 50.0 * delta
+	if plane.scale.x < 0.7:
+		plane.scale.x += 0.1 * delta
+		plane.scale.y += 0.1 * delta
 	helicopter.global_position.x += 20.0 * delta	# it's a child node
+	helicopter.global_position.y -= 10.0 * delta	# it's a child node
+	carrier.position.x -= 10.0 * delta
+	carrier.position.y += 10.0 * delta

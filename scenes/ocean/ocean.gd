0abe2c4ec9a006ddae10c2f14a736776f30c5ff0
MohebYarahmadi <moheb.yarahmadi@gmail.com>
2026-02-20 15:58:13 +0330

    Foundation 11 Access by Unique Name

--- scenes/ocean/ocean.gd
+++ scenes/ocean/ocean.gd
@@ -3,7 +3,10 @@ extends Node2D
 
 @onready var plane: Sprite2D = $plane
 @onready var carrier: Sprite2D = $carrier
-@onready var helicopter: Sprite2D = $carrier/helicopter
+
+# Access as Unique Name option on for helicopter (recomended) % sign
+# ...now it doesn't matter the hierarchy of helicopter.
+@onready var helicopter: Sprite2D = %helicopter
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,7 +20,7 @@ func _process(delta: float) -> void:
 	if plane.scale.x < 0.7:
 		plane.scale.x += 0.1 * delta
 		plane.scale.y += 0.1 * delta
-	helicopter.global_position.x += 20.0 * delta	# it's a child node
-	helicopter.global_position.y -= 10.0 * delta	# it's a child node
+	helicopter.position.x += 20.0 * delta	# it's a child node
+	helicopter.position.y -= 10.0 * delta	# it's a child node
 	carrier.position.x -= 10.0 * delta
 	carrier.position.y += 10.0 * delta

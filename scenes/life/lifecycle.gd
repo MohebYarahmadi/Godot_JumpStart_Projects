e1f6e60260b7a868465ae7d8c69758c4a479f3e2
MohebYarahmadi <moheb.yarahmadi@gmail.com>
2026-02-26 11:17:38 +0330

    Godot - FPS Done

--- scenes/life/lifecycle.gd
+++ scenes/life/lifecycle.gd
@@ -10,4 +10,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	print("_process: ", delta)
+
+
+func _physics_process(delta: float) -> void:
+	print("_physics_process: ", delta)
+	

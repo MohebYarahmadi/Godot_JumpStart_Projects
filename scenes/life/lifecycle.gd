1c76411dd983d752b42db266d44ee38428f9c644
MohebYarahmadi <moheb.yarahmadi@gmail.com>
2026-02-26 11:45:25 +0330

    Godot - Scenes Done

--- scenes/life/lifecycle.gd
+++ scenes/life/lifecycle.gd
@@ -1,18 +1,3 @@
 extends Node2D
 
-
-@onready var test: Node2D = $test
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	print("_process: ", delta)
-
-
-func _physics_process(delta: float) -> void:
-	print("_physics_process: ", delta)
-	
+@onready var hobbit: Hobbit = $hobbit

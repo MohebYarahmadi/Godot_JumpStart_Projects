1608005ff4be46b3a03657c7e2d3b87cbaee6dd9
MohebYarahmadi <moheb.yarahmadi@gmail.com>
2026-03-01 09:41:35 +0330

    Godot - Signals Part2 DONE

--- scenes/life/lifecycle.gd
+++ scenes/life/lifecycle.gd
@@ -4,6 +4,10 @@ extends Node2D
 @onready var wizard: Wizard = $wizard
 
 
+func _ready() -> void:
+	# make connection to signals internal and share with other instances.
+	hobbit.kill_wizard.connect(_on_hobbit_kill_wizard)
+
 func _on_wizard_cast_spell() -> void:
 	hobbit.hit_by_spell()
 
